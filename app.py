818885a95f57db4c66bc5d7366c1285ce1ebd5c5
Auria24 <ilmasy_24_887@hotmail.com>
2024-08-18 20:02:20 -0600

    creacion de graficas

---
+++ app.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import streamlit as st
+import plotly_express as px
+
+datos = pd.read_csv('vehicles_us.csv')
+
+hist_button = st.button('Construir histograma') # crear un botón
+        
+if hist_button: # al hacer clic en el botón
+            # escribir un mensaje
+            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            # crear un histograma
+            fig = px.histogram(datos, x="odometer")
+            # mostrar un gráfico Plotly interactivo
+            st.plotly_chart(fig, use_container_width=True)
+
+
+build_histograma = st.checkbox('Construir un histograma')
+
+if build_histograma: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+    
+
+scatter_button = st.button('Construir histograma') # crear un botón   
+if scatter_button: # al hacer clic en el botón
+            # escribir un mensaje
+            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')   
+            # crear un histograma
+            fig = px.scatter(datos, x="odometer")
+            # mostrar un gráfico Plotly interactivo
+            st.plotly_chart(fig, use_container_width=True)
